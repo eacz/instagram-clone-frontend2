d22b1c11ea73e4d55ed66cb14f3e3b497f38861e
Esteban Canteros <estebanacanteros@gmail.com>
2022-01-06 23:25:43 -0300

    initial i18n config made

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store'
+import './config/i18n'
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { setDarkTheme, setLightTheme } from '../store/theme/slice';
 import { useAppDispatch } from '../hooks/index';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {  
   const dispatch = useAppDispatch()
+  const { t } = useTranslation()
   return (
     <div>
-      <h1>Home</h1>
+      <h1>{t('home')}</h1>
       <button onClick={() => dispatch(setLightTheme())}>Light</button>
       <button onClick={() => dispatch(setDarkTheme())}>Dark</button>
     </div>
