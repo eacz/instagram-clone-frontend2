2a049e03f1b63a55f4811f61d1d0cf41fc34e15a
Esteban Canteros <ecanteros.churrasco@wallypos.com>
2022-08-26 01:27:04 -0300

    some global app changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,15 +4,17 @@ import { useTranslation } from 'react-i18next'
 import { lngs } from '../config/i18n'
 import { Link } from 'react-router-dom'
 import { logout } from '../store/auth/slice'
+import { Post } from '../components/posts'
 
 const HomePage = () => {
   const dispatch = useAppDispatch()
   const { t, i18n } = useTranslation()
   return (
     <div>
+      <Post />
       <h1>{t('home')}</h1>
       <div>
-        {Object.entries(lngs).map(([ key, value ]) => (
+        {Object.entries(lngs).map(([key, value]) => (
           <button
             key={key}
             type='submit'
@@ -24,11 +26,13 @@ const HomePage = () => {
       </div>
       <button onClick={() => dispatch(setLightTheme())}>Light</button>
       <button onClick={() => dispatch(setDarkTheme())}>Dark</button>
-      <p>{t('interpolation', {something: 'this is an interpolation example'})}</p>
-      <p>{t('interpolation', {something: 'same as above'})}</p>
-      <Link to="/accounts/register">Register</Link>
-      <Link to="/accounts/login">Login</Link>
-      <button onClick={() => dispatch(logout())} >logout</button>
+      <p>
+        {t('interpolation', { something: 'this is an interpolation example' })}
+      </p>
+      <p>{t('interpolation', { something: 'same as above' })}</p>
+      <Link to='/accounts/register'>Register</Link>
+      <Link to='/accounts/login'>Login</Link>
+      <button onClick={() => dispatch(logout())}>logout</button>
     </div>
   )
 }
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -2,33 +2,36 @@ import { DefaultTheme } from 'styled-components'
 
 export type themeTypes = 'light' | 'dark'
 
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme {
-    type: themeTypes, 
-    background: string;
-    primaryColor: string;
-    fontColor:  string;
-    backgroundContrast: string;
-    error: string;
+    type: themeTypes
+    background: string
+    primaryColor: string
+    fontColor: string
+    backgroundContrast: string
+    error: string
+    globalParagraphSize: string
     //secondaryColor: string;
     //backgroundContrast: string;
   }
 }
 
-export const lightTheme : DefaultTheme = {
+export const lightTheme: DefaultTheme = {
   type: 'light',
   background: '#ffffff',
   primaryColor: '#0095f6',
   fontColor: '#262626',
   backgroundContrast: '#dbdbdb',
-  error: '#ff3749'
+  error: '#ff3749',
+  globalParagraphSize: '14px'
 }
 
-export const darkTheme : DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   type: 'dark',
   background: '#1f1f1f',
   primaryColor: '#0095f6',
   fontColor: '#ffffff',
   backgroundContrast: '#dbdbdb',
-  error: '#ff3749'
+  error: '#ff3749',
+  globalParagraphSize: '14px'
 }
