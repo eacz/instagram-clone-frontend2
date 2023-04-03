323f93d022ce8178ccb01659d7898820ac6057ca
Esteban Canteros <estebanacanteros@gmail.com>
2023-04-03 08:31:19 -0300

    add props of fontSize and enableHover on TextButton

--- src/components/common/TextButton.tsx
+++ src/components/common/TextButton.tsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components'
 
-const TextButton = styled.button`
-  color: ${(p) =>
-    p.disabled ? p.theme.disabledFontColor : p.theme.primaryColor};
+interface Props {
+  fontSize?: number
+  changeColorOnHover?: boolean
+}
+
+const TextButton = styled.button<Props>`
+  color: ${(p) => (p.disabled ? p.theme.disabledFontColor : p.theme.primaryColor)};
   background-color: transparent;
   cursor: ${(p) => (p.disabled ? 'default' : 'pointer')};
   border: none;
   font-weight: 700;
+  font-size: ${(p) => (p.fontSize ? p.fontSize : 13)}px;
+  transition: all 0.5s ease;
+  &:hover {
+    color: ${(p) => (p.changeColorOnHover ? p.theme.fontColor : p.theme.primaryColor)};
+    transition: all 0.5s ease;
+  }
 `
 
 export default TextButton
